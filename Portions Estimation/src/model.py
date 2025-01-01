5160d715240ca28490e518edd0c6dc1be874b8d0
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-01-01 13:02:25 +0200

    Add ResNet101 model and update dataset documentation
    Introduced a ResNet101 CNN model with support for 7 input channels, designed to predict glycemic load.
    
    Updated dataset documentation to include new files, additional details on ingredients, and processing steps for glycemic load estimation.

--- Portions Estimation/src/model.py
+++ Portions Estimation/src/model.py
@@ -1,1 +1,11 @@
 # The CNN models definition
+import torchvision.models as models
+import torch.nn as nn
+
+def create_resnet101():
+    resnet101 = models.resnet101(pretrained=True)
+    resnet101.conv1 = nn.Conv2d(7, 64, kernel_size=7, stride=2, padding=3, bias=False) # RGB+RGBD (7 channels total)
+    num_features = resnet101.fc.in_features
+    resnet101.fc = nn.Linear(num_features, 1)  # Predict glycemic load
+    return resnet101
+
