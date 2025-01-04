4dce9f533f5c6468ad1ebf3e034101917123a103
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-01-04 12:01:58 +0200

    Refactor preprocessing to separate RGB and depth handling.
    Revised the RGB and depth preprocessing pipelines for clarity and efficiency. Updated tensor normalization for consistency and replaced combined RGBD preprocessing with standalone depth processing. Adjusted file paths and variable names to match the new structure.

--- Portions Estimation/src/data_preprocessing.py
+++ Portions Estimation/src/data_preprocessing.py
@@ -7,11 +7,10 @@ from tqdm import tqdm
 import torchvision.transforms as transforms
 from PIL import Image
 
-
 BUCKET_NAME = "nutrition5k_dataset"
 REALSENSE_OVERHEAD_PATH = "nutrition5k_dataset/imagery/realsense_overhead/"
 CURRENT_DIR = os.path.dirname(os.getcwd())
-PROCESSED_DATA_DIR = os.path.join(CURRENT_DIR, r"data\processed_combined")
+PROCESSED_DATA_DIR = os.path.join(CURRENT_DIR, r"data\processed")
 RAW_DATA_DIR = os.path.join(CURRENT_DIR, r"data\raw")
 INGREDIENTS_METADATA_FILEPATH = os.path.join(CURRENT_DIR, r"data/ingredients.csv")
 
@@ -30,37 +29,25 @@ def download_image_from_gcs(bucket_name, source_blob_name, local_temp_path) -> s
     blob.download_to_filename(local_temp_path)
     return local_temp_path
 
-def preprocess_rgb(image_raw, target_size=(224, 224)) -> torch.Tensor:
-    # Normalize using ImageNet means and standard deviations
+def preprocess_rgb(image_path, target_size=(224, 224)) -> torch.Tensor:
     transform = transforms.Compose([
         transforms.Resize(target_size),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.0, 0.0, 0.0],
+                             std=[1.0, 1.0, 1.0])
     ])
-    image = Image.open(image_raw).convert("RGB")
+    image = Image.open(image_path).convert("RGB")
     return transform(image)
 
 def preprocess_rgbd(image_path, target_size=(224, 224)) -> torch.Tensor:
-    image = Image.open(image_path).convert("RGBA")
-    rgb_image = image.split()[:3]
-    depth_channel = image.split()[3]
-    transform_rgb = transforms.Compose([
+    depth_transform = transforms.Compose([
         transforms.Resize(target_size),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.0], std=[1.0])
     ])
-    rgb_tensor = transform_rgb(Image.merge("RGB", rgb_image))
-
-    # Depth
-    transform_depth = transforms.Compose([
-        transforms.Resize(target_size),
-        transforms.ToTensor()
-    ])
-    depth_tensor = transform_depth(depth_channel)
-    rgbd_tensor = torch.cat((rgb_tensor, depth_tensor), dim=0) # Combine both images
-    return rgbd_tensor
+    depth_image = Image.open(image_path).convert("L")
+    depth_tensor = depth_transform(depth_image)
+    return depth_tensor
 
 
 def get_glycemic_load(dish_id):
@@ -85,24 +72,24 @@ def preprocess_dataset(annotations_file, target_size=(224, 224), mode='split'):
     for _, row in tqdm(annotations.iterrows(), total=len(annotations)):
         dish_id = row.iloc[0]
         gcs_image_path_rgb = f"{REALSENSE_OVERHEAD_PATH}{dish_id}/rgb.png"
-        gcs_image_path_rgbd = f"{REALSENSE_OVERHEAD_PATH}{dish_id}/depth_color.png"
+        gcs_image_path_depth = f"{REALSENSE_OVERHEAD_PATH}{dish_id}/depth_raw.png"
         local_temp_path_rgb = "temp_image_rgb.jpg"
-        local_temp_path_rgbd = "temp_image_rgbd.jpg"
+        local_temp_path_depth = "temp_image_depth.jpg"
 
         # Downloading the image and appending new info
         try:
             download_image_from_gcs(BUCKET_NAME, gcs_image_path_rgb, local_temp_path_rgb)
-            download_image_from_gcs(BUCKET_NAME, gcs_image_path_rgbd, local_temp_path_rgbd)
-            preprocessed_image_rgb = preprocess_rgb(local_temp_path_rgb, target_size)
-            preprocessed_image_rgbd = preprocess_rgbd(local_temp_path_rgbd, target_size)
+            download_image_from_gcs(BUCKET_NAME, gcs_image_path_depth, local_temp_path_depth)
+            preprocessed_rgb_tensor = preprocess_rgb(local_temp_path_rgb, target_size)
+            preprocessed_depth_tensor = preprocess_rgbd(local_temp_path_depth, target_size)
 
             gl = get_glycemic_load(dish_id)
 
             match mode:
                 case 'combined':
-                    combined_preprocessed_image = torch.cat((preprocessed_image_rgb, preprocessed_image_rgbd), dim=0)  # Shape: [7, H, W]
+                    combined_preprocessed_tensor = torch.cat((preprocessed_rgb_tensor, preprocessed_depth_tensor), dim=0)  # Shape: [4, H, W]
                     output_combined_path = os.path.join(PROCESSED_DATA_DIR, f"combined_{dish_id}.pt")
-                    torch.save(combined_preprocessed_image, output_combined_path)
+                    torch.save(combined_preprocessed_tensor, output_combined_path)
                     preprocessed_data.append({
                         "image_id": dish_id,
                         "processed_combined_image_path": output_combined_path,
@@ -110,25 +97,25 @@ def preprocess_dataset(annotations_file, target_size=(224, 224), mode='split'):
                     })
                 case 'split':
                     output_path_rgb = os.path.join(PROCESSED_DATA_DIR, f"rgb_{dish_id}.pt")
-                    output_path_rgbd = os.path.join(PROCESSED_DATA_DIR, f"rgbd_{dish_id}.pt")
-                    torch.save(preprocessed_image_rgb, output_path_rgb)
-                    torch.save(preprocessed_image_rgbd, output_path_rgbd)
+                    output_path_depth = os.path.join(PROCESSED_DATA_DIR, f"rgbd_{dish_id}.pt")
+                    torch.save(preprocessed_rgb_tensor, output_path_rgb)
+                    torch.save(preprocessed_depth_tensor, output_path_depth)
                     preprocessed_data.append({
                         "image_id": dish_id,
-                        "processed_image_path_rgb": output_path_rgb,
-                        "processed_imgae_path_rgbd": output_path_rgbd,
+                        "processed_path_rgb_tensor": output_path_rgb,
+                        "processed_path_depth_tensor": output_path_depth,
                         "glycemic load": gl
                     })
 
         finally:
             rgb_full_path = os.path.join(os.getcwd(), local_temp_path_rgb)
-            rgbd_full_path = os.path.join(os.getcwd(), local_temp_path_rgbd)
+            depth_full_path = os.path.join(os.getcwd(), local_temp_path_depth)
             if os.path.exists(rgb_full_path):
                 os.remove(rgb_full_path)
-            if os.path.exists(rgbd_full_path):
-                os.remove(rgbd_full_path)
+            if os.path.exists(depth_full_path):
+                os.remove(depth_full_path)
 
-    pd.DataFrame(preprocessed_data).to_csv(os.path.join(PROCESSED_DATA_DIR, "processed_combined_annotations.csv"), index=False)
+    pd.DataFrame(preprocessed_data).to_csv(os.path.join(PROCESSED_DATA_DIR, "processed_annotations.csv"), index=False)
 
 
 
