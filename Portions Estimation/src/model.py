20fc7167bdcc12a8bb5af3aca2869e970e72f2b1
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-01-05 20:52:04 +0200

    Add ResNet models for RGB and RGBD input support
    Introduced ResNet18, ResNet34, ResNet50, and ResNet101 classes with custom modifications to support RGB and RGBD inputs. Modified the first convolutional layers to handle 4 input channels and adjusted output layers for single-value predictions. Ensured usage of pretrained weights for initialization.

--- Portions Estimation/src/model.py
+++ Portions Estimation/src/model.py
@@ -1,6 +1,7 @@
 # The CNN models definition
 import torchvision.models as models
 import torch.nn as nn
+from torchvision.models import ResNet50_Weights, ResNet101_Weights, ResNet34_Weights, ResNet18_Weights
 
 
 class ResNet101WithRGBandRGBD(nn.Module):
@@ -8,18 +9,77 @@ class ResNet101WithRGBandRGBD(nn.Module):
         super(ResNet101WithRGBandRGBD, self).__init__()
 
         # Load ResNet101 pretrained model
-        self.resnet = models.resnet101(pretrained=True)
+        self.resnet = models.resnet101(weights=ResNet101_Weights.DEFAULT)
 
         # Modify the first convolutional layer to accept 7 input channels (3 from RGB, 4 from RGBD)
-        self.resnet.conv1 = nn.Conv2d(7, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3), bias=False)
+        self.resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3), bias=False)
 
         self.resnet.fc = nn.Linear(in_features=2048, out_features=1)  # Output glycemic load (single value)
-
-        print(self)
 
 
     def forward(self, combined_input):
         # Pass the combined input through ResNet
         out = self.resnet(combined_input).squeeze(dim=1) # Outputs tensor shape: (batch_size, 1) -> (batch_size)
         return out
+
+class ResNet50WithRGBandRGBD(nn.Module):
+    def __init__(self):
+        super(ResNet50WithRGBandRGBD, self).__init__()
+
+        # Load ResNet101 pretrained model
+        self.resnet = models.resnet50(weights=ResNet50_Weights.DEFAULT)
+
+        # Modify the first convolutional layer to accept 7 input channels (3 from RGB, 4 from RGBD)
+        self.resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3), bias=False)
+
+        self.resnet.fc = nn.Linear(in_features=2048, out_features=1)  # Output glycemic load (single value)
+
+
+    def forward(self, combined_input):
+        # Pass the combined input through ResNet
+        out = self.resnet(combined_input).squeeze(dim=1) # Outputs tensor shape: (batch_size, 1) -> (batch_size)
+        return out
+
+class ResNet34WithRGBandRGBD(nn.Module):
+    def __init__(self):
+        super(ResNet34WithRGBandRGBD, self).__init__()
+
+        # Load ResNet101 pretrained model
+        self.resnet = models.resnet34(weights=ResNet34_Weights.DEFAULT)
+
+        # Modify the first convolutional layer to accept 7 input channels (3 from RGB, 4 from RGBD)
+        self.resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3), bias=False)
+
+        self.resnet.fc = nn.Linear(in_features=512, out_features=1)  # Output glycemic load (single value)
+
+
+    def forward(self, combined_input):
+        # Pass the combined input through ResNet
+        out = self.resnet(combined_input).squeeze(dim=1) # Tensor shape: (batch_size, 1) -> (batch_size)
+        return out
+
+class ResNet18WithRGBandRGBD(nn.Module):
+    def __init__(self):
+        super(ResNet18WithRGBandRGBD, self).__init__()
+
+        # Load ResNet101 pretrained model
+        self.resnet = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+
+        # Modify the first convolutional layer to accept 7 input channels (3 from RGB, 4 from RGBD)
+        self.resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3), bias=False)
+
+        self.resnet.fc = nn.Linear(in_features=512, out_features=1)  # Output glycemic load (single value)
+
+
+    def forward(self, combined_input):
+        # Pass the combined input through ResNet
+        out = self.resnet(combined_input).squeeze(dim=1) # Tensor shape: (batch_size, 1) -> (batch_size)
+        return out
+
+    def print(self):
+        print(self.resnet)
+
+if __name__ == '__main__':
+    model = ResNet18WithRGBandRGBD()
+    model.print()
 
