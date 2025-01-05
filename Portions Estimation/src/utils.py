2db3cce41a0cb38eb2253f82ddc4b8eb70bbed5d
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-01-05 15:22:47 +0200

    Add data augmentation and improve loss plotting
    Introduced transformations for data augmentation, including random flips, rotations, and color jittering, ensuring both RGB and depth tensors are augmented consistently. Updated the loss plotting function for better clarity with detailed x-axis labeling and printed loss arrays for debugging.

--- Portions Estimation/src/utils.py
+++ Portions Estimation/src/utils.py
@@ -5,6 +5,8 @@ import pandas as pd
 import torch
 from torch.utils.data import random_split
 import matplotlib.pyplot as plt
+import torchvision.transforms as T
+
 
 class MealDataset(Dataset):
     def __init__(self, csv_file, transform=None):
@@ -20,12 +22,32 @@ class MealDataset(Dataset):
         glycemic_load = self.data.iloc[idx, 2]
 
         # Load the RGB and RGBD tensors
-        combined_tensor = torch.load(combined_path) # Shape: [7, H, W]
+        combined_tensor = torch.load(combined_path) # Shape: [4, H, W]
+
+        # Separate RGB and depth channels
+        rgb_tensor = combined_tensor[:3]  # Shape: [3, H, W]
+        depth_tensor = combined_tensor[3:]  # Shape: [1, H, W]
+
+        # Apply augmentations to the RGB tensor
+        if self.transform:
+            augmented_rgb = self.transform(rgb_tensor)
+
+            # Ensure depth tensor matches RGB transformations
+            depth_tensor = self.transform(depth_tensor)
+
+        # Combine RGB and depth tensors back
+        combined_tensor = torch.cat((augmented_rgb, depth_tensor), dim=0)
 
         return combined_tensor, glycemic_load
 
 
-def get_data_loaders(csv_file, batch_size=32, transform=None, val_size=0.2, test_size=0.2):
+def get_data_loaders(csv_file, batch_size=32, val_size=0.2, test_size=0.2):
+    transform = T.Compose([
+        T.RandomHorizontalFlip(p=0.5),
+        T.RandomRotation(degrees=15),
+        T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1), # Expects [1 or 3, H , W]
+    ])
+
     dataset = MealDataset(csv_file=csv_file, transform=transform)
 
     # Split proportions
@@ -45,11 +67,13 @@ def get_data_loaders(csv_file, batch_size=32, transform=None, val_size=0.2, test
 
 def plot_loss_curve(train_loss, val_loss, num_epochs, save_path=None):
     """Plot training and validation loss curves."""
-    x = np.arange(1, num_epochs)
+    print(f'The train loss array is: {train_loss}')
+    print(f'The validation loss array is: {val_loss}')
+    x = np.arange(num_epochs)
     plt.plot(x,train_loss, label="Train Loss")
     plt.plot(x, val_loss, label="Validation Loss")
     plt.xlabel("Epochs")
-    plt.xticks(x)
+    plt.xticks(np.arange(0, num_epochs, 5), fontsize=10)
     plt.ylabel("Loss")
     plt.legend()
     plt.title("Loss Curve")
