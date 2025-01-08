1fd4f75175640fffd357885258ce465494e6f0c6
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-01-08 15:47:34 +0200

    Refactor training script for improved flexibility and utility
    Updated the `train` function to accept model, hyperparameters, and experiment name as arguments, enhancing reusability. Added support for ResNet18, AdamW optimizer, and model saving via `save_model`. Removed fixed hyperparameters and partially implemented early stopping logic for future integration.

--- Portions Estimation/src/train.py
+++ Portions Estimation/src/train.py
@@ -1,33 +1,35 @@
 # Script to train the models
 import torch
 import torch.nn as nn
-from model import ResNet34WithRGBandRGBD
-from utils import get_data_loaders, plot_loss_curve
+from model import ResNet34WithRGBandRGBD, ResNet18WithRGBandRGBD
+from utils import get_data_loaders, save_model, load_model
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 
-def train(experiment_name):
+
+
+def train(model, experiment_name, batch_size, num_epochs, learning_rate, weight_decay):
+    # Initialize general
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    writer = SummaryWriter(f"runs/{experiment_name}")
+    writer = SummaryWriter(f"runs-updated/{experiment_name}")
+    model.to(device)
     # Hyperparameters
-    batch_size = 32
-    num_epochs = 3
-    learning_rate = 0.001
+    # min_delta = 0.001
+    # patience = 20
     csv_file = r"C:\Users\rotem.geva\PycharmProjects\GlycemicLoad\Portions Estimation\data\processed\processed_annotations.csv"
 
     # Prepare data
     train_loader, val_loader, test_loader = get_data_loaders(csv_file, batch_size)
 
-    # Initialize model
-    model = ResNet34WithRGBandRGBD().to(device)
-
     # Loss and optimizer
     criterion = nn.MSELoss()  # use MSELoss for regression
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+    # # Early stopping mechanism
+    # best_val_loss = float('inf')
+    # patience_counter = 0
 
     # Training loop
-    losses = []
-    val_losses = []
     for epoch in tqdm(range(num_epochs), desc="Training Epochs"):
         model.train()  # Set the model to training mode
         for combined_tensor, glycemic_load in train_loader:
@@ -45,7 +47,7 @@ def train(experiment_name):
             loss.backward()
             optimizer.step()
 
-        # Validation loop - evaluate every few epochs
+        # Validation loop
         model.eval()
         with torch.no_grad():
             val_loss = 0.0
@@ -59,20 +61,33 @@ def train(experiment_name):
         curr_loss = loss.item()
         curr_val_loss = val_loss / len(val_loader)
         print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {curr_loss:.4f}, Val Loss: {curr_val_loss:.4f}")   # Average validation loss per batch
-        losses.append(curr_loss)
-        val_losses.append(curr_val_loss)
+
+        # Write to Tensorboard
         writer.add_scalar('Loss/train', curr_loss, epoch)
         writer.add_scalar('Loss/validation', curr_val_loss, epoch)
 
-    # Plot loss
-    plot_loss_curve(train_loss=losses, val_loss=val_losses, num_epochs=num_epochs)
+        # # Early stopping logic
+        # if curr_val_loss < best_val_loss - min_delta:
+        #     best_val_loss = curr_val_loss
+        #     patience_counter = 0  # Reset patience counter
+        # else:
+        #     patience_counter += 1
+        #     print(f"No improvement in validation loss for {patience_counter} epochs.")
+        #     if patience_counter >= patience:
+        #         print("Early stopping triggered. Stopping training.")
+        #         break
 
     # Optionally save the trained model
     saving_option = input('Would you like to save the model?')
     if saving_option.lower() == 'y':
-        torch.save(model.state_dict(), 'model.pth')
-
+        save_model(model, f"trained_models/{experiment_name}.pth", num_epochs, optimizer, loss)
     writer.close()
 
 if __name__ == "__main__":
-    train()
+    model = ResNet18WithRGBandRGBD(is_pretrained=False)
+    experiment_name = "untrained_resnet18_batch32_0.00001_lr"
+    batch_size = 32
+    num_epochs = 800
+    learning_rate = 0.00001
+    weight_decay = 0.0
+    train(model, experiment_name, batch_size, num_epochs, learning_rate, weight_decay)
