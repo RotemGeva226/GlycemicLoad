017ae3b2f3cc915a7f2a7ba38b70f10433e0d52d
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-01-11 22:44:50 +0200

    Refactor preprocessing and enhance glycemic index filtering
    Added type hints and docstrings to improve code readability and maintainability. Introduced filtering to exclude ingredients without a glycemic index, refining dataset preparation. Updated file naming conventions for consistent output.

--- Portions Estimation/src/ingredients_preprocessing.py
+++ Portions Estimation/src/ingredients_preprocessing.py
@@ -34,7 +34,14 @@ def add_unique_items(list1, list2) -> list:
     return list1
 
 
-def extract_ingredient_content(row, ingredient, mode):
+def extract_ingredient_content(row, ingredient, mode) -> str or None:
+    """
+    This function extracts the nutritional content of an ingredient from a row.
+    :param row: contains ingredients from a row.
+    :param ingredient: name of the ingredient.
+    :param mode: mass or carbs.
+    :return: nutritional content of the ingredient.
+    """
     add = 0
     match mode:
         case 'mass':
@@ -47,29 +54,37 @@ def extract_ingredient_content(row, ingredient, mode):
         return ls[index + add]
 
 
-def preprocess_ingredients_dataset(annotations_file, metadata_file):
+def preprocess_ingredients_dataset(annotations_file, metadata_file) -> None:
+    """
+    This function preprocesses the ingredients dataset.
+    :param annotations_file: the input file that contains all dishes metadata from Nutrition5k dataset.
+    :param metadata_file: the input file that contains all ingredients metadata from Nutrition5k dataset.
+    """
     annotations = pd.read_csv(annotations_file, header=None)
     metadata = pd.read_csv(metadata_file)
-    sauces = metadata[metadata['IsSauce'] == 'Yes']['ingr'].tolist()
+    metadata_filtered = metadata.dropna(subset=['Glycemic Index']) # Remove ingredients without glycemic index.
+    sauces = metadata_filtered[metadata_filtered['IsSauce'] == 'Yes']['ingr'].tolist()
+    remained_ingredients = metadata_filtered['ingr'].tolist() # Lists the ingredients that were left after removing the
+    # ones without glycemic index.
     res = pd.DataFrame(
         columns=["Dish ID", "Ingredient Name", "Mass (g)", "Carbs (g)", "Glycemic Index", "Glycemic Load"])
     for _, row in tqdm(annotations.iterrows(), total=len(annotations)):
         dish_id = row.iloc[0]
         ingredients = extract_ingredients(row, sauces)
         for ingredient in ingredients:
-            if ingredient not in sauces:
+            if ingredient in remained_ingredients and ingredient not in sauces:
                 mass = extract_ingredient_content(row, ingredient, mode='mass')
                 carbs = extract_ingredient_content(row, ingredient, mode='carbs')
-                glycemic_index = metadata[metadata['ingr'] == ingredient]['Glycemic Index'].iloc[0]
+                glycemic_index = metadata_filtered[metadata_filtered['ingr'] == ingredient]['Glycemic Index'].iloc[0]
                 glycemic_load = carbs * glycemic_index / 100
                 new_row_data = {'Dish ID': dish_id, 'Ingredient Name': ingredient, "Mass (g)": mass, "Carbs (g)": carbs,
                                 "Glycemic Index": glycemic_index, "Glycemic Load": glycemic_load}
                 res.loc[len(res)] = new_row_data
-    res.to_csv(os.path.join(os.path.dirname(os.getcwd()), r'data/preprocessed/ingredients.csv'), index=False)
+    res.to_csv(os.path.join(os.path.dirname(os.getcwd()), r'data/ingredients-110124.csv'), index=False)
 
 if __name__ == '__main__':
     dirname = os.path.dirname(os.getcwd())
-    input_filepath = os.path.join(dirname, r"data/raw/Nutrition5kModified700.csv")
+    input_filepath = os.path.join(dirname, r"data/raw/nutrition5k_dataset_metadata_dish_metadata_cafe1.csv")
     sauces_filepath = os.path.join(dirname, r"data/raw/nutrition5k_dataset_metadata_ingredients_metadata.csv")
     preprocess_ingredients_dataset(input_filepath, sauces_filepath)
 
