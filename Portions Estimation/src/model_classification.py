3492929fddc3e68acb9aa3f0c8a1d82c7c481b35
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-01-16 10:18:55 +0200

    Refactor ResNet34 and modify softmax implementation.
    Refactored the ResNet34 model to simplify its structure, removing unused components and ensuring a consistent interface. Updated softmax activation in both ResNet18 and ResNet34 to use `nn.Softmax` instead of `F.softmax` for improved readability and alignment with modern PyTorch practices. These changes streamline the model definitions and enhance code clarity.

--- Portions Estimation/src/model_classification.py
+++ Portions Estimation/src/model_classification.py
@@ -1,27 +1,24 @@
 # The CNN models definition
 import torchvision.models as models
 import torch.nn as nn
-from torchvision.models import ResNet18_Weights
-import torch.nn.functional as F
+from torchvision.models import ResNet18_Weights, ResNet34_Weights
 
 
-class ResNet34WithRGBandRGBD(nn.Module):
-    def __init__(self, is_pretrained):
-        super(ResNet34WithRGBandRGBD, self).__init__()
+class ResNet34(nn.Module):
+    def __init__(self, num_classes):
+        super(ResNet34, self).__init__()
 
-        # Load ResNet101 pretrained model
-        self.resnet = models.resnet34(pretrained=is_pretrained)
+        # Load Resnet18 pretrained model
+        self.resnet = models.resnet34(weights=ResNet34_Weights.DEFAULT)
 
-        # Modify the first convolutional layer to accept 7 input channels (3 from RGB, 4 from RGBD)
-        self.resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(3, 3), stride=(2, 2), padding=(3, 3), bias=False)
+        self.resnet.fc = nn.Linear(in_features=512,
+                                   out_features=num_classes)  # Output glycemic load group (low, medium, high)
 
-        self.resnet.fc = nn.Linear(in_features=512, out_features=1)  # Output glycemic load (single value)
-
-
-    def forward(self, combined_input):
+    def forward(self, x):
         # Pass the combined input through ResNet
-        out = self.resnet(combined_input).squeeze(dim=1) # Tensor shape: (batch_size, 1) -> (batch_size)
-        return out
+        x = self.resnet(x)
+        x = nn.Softmax(dim=1)(x)
+        return x
 
 class ResNet18(nn.Module):
     def __init__(self, num_classes):
@@ -36,7 +33,7 @@ class ResNet18(nn.Module):
     def forward(self, x):
         # Pass the combined input through ResNet
         x = self.resnet(x)
-        x = F.softmax(x, dim=1)
+        x = nn.Softmax(dim=1)(x)
         return x
 
 
