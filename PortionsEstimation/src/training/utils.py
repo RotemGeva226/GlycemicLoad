74b815b775fa8650b07b25da823c5cb0e3fdbd6d
Rotem.Geva <rotem.geva@s.afeka.ac.il>
2025-04-10 11:18:00 +0300

    Add load_test_loader function to handle the bug of saving incorrect test loader

--- PortionsEstimation/src/training/utils.py
+++ PortionsEstimation/src/training/utils.py
@@ -1,5 +1,11 @@
 # Utility functions (e.g., for saving/loading models, metrics)
+import os.path
+
 import torch
+from torch.utils.data import DataLoader
+
+from data.MealDataset import MealDataset
+
 
 def save_model(model, model_path, epoch, optimizer, loss):
     try:
@@ -20,6 +26,16 @@ def load_model(model_path, model_class, model_args: dict=None):
         return model
     except Exception as e:
         print('An error occurred while loading the model:', e)
+
+def load_test_loader(batch_size, tl_name):
+    tl_folderpath = r"C:\Users\rotem.geva\PycharmProjects\GlycemicLoad\PortionsEstimation\src\models/saved/tl-single_ingr_portions_regression"
+    csv_file = r"C:\Users\rotem.geva\PycharmProjects\GlycemicLoad\PortionsEstimation\src\data\processed\processed_single_ingr_portions_regression\processed_single_ingr_portions_regression.csv"
+    dataset = MealDataset(csv_file)
+    train_loader, val_loader, test_loader = dataset.get_regression_loader(batch_size=batch_size)
+    torch.save(test_loader.dataset,os.path.join(tl_folderpath, tl_name + ".pt"))
+    test_dataset = torch.load(os.path.join(tl_folderpath, tl_name + ".pt"))
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return test_loader
 
 if __name__ == "__main__":
     load_model(
